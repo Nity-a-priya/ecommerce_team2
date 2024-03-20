86f23a907f9d5f31ea89efcba0582001fcf8a483
Nity-a-priya <nityapriya.katta@thoughtworks.com>
2024-03-20 17:41:02 +0530

    fix ionicon error

--- App.js
+++ App.js
@@ -1,19 +1,19 @@
-import React, { useContext } from "react";
-import { View, StatusBar, SafeAreaView } from "react-native";
+import React, {useContext} from 'react';
+import {View, StatusBar, SafeAreaView} from 'react-native';
 
-import Login from "./src/screens/Login";
-import HomeScreen from "./src/screens/HomeScreen";
-import WishlistScreen from "./src/screens/WishlistScreen";
-import CartScreen from "./src/screens/CartScreen";
-import SettingsScreen from "./src/screens/SettingsScreen";
+import Login from './src/screens/Login';
+import HomeScreen from './src/screens/HomeScreen';
+import WishlistScreen from './src/screens/WishlistScreen';
+import CartScreen from './src/screens/CartScreen';
+import SettingsScreen from './src/screens/SettingsScreen';
 
-import { NavigationContainer } from "@react-navigation/native";
-import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
-import { createStackNavigator } from "@react-navigation/stack";
+import {NavigationContainer} from '@react-navigation/native';
+import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
+import {createStackNavigator} from '@react-navigation/stack';
 
-import NameContextProvider from "./src/Utils/name-context";
-import { NameContext } from "./src/Utils/name-context";
-
+import NameContextProvider from './src/Utils/name-context';
+import {NameContext} from './src/Utils/name-context';
+import Ionicons from 'react-native-vector-icons/Ionicons';
 
 const Tab = createBottomTabNavigator();
 const Stack = createStackNavigator();
@@ -21,10 +21,42 @@ const Stack = createStackNavigator();
 const AuthScreens = () => {
   return (
     <Tab.Navigator>
-      <Tab.Screen name="Home" component={HomeScreen} />
-      <Tab.Screen name="Wishlist" component={WishlistScreen} />
-      <Tab.Screen name="Cart" component={CartScreen} />
-      <Tab.Screen name="Settings" component={SettingsScreen} />
+      <Tab.Screen
+        name="Home"
+        component={HomeScreen}
+        options={{
+          tabBarIcon: ({color, size}) => (
+            <Ionicons name="home-outline" color={color} size={size} />
+          ),
+        }}
+      />
+      <Tab.Screen
+        name="Wishlist"
+        component={WishlistScreen}
+        options={{
+          tabBarIcon: ({color, size}) => (
+            <Ionicons name="heart-outline" color={color} size={size} />
+          ),
+        }}
+      />
+      <Tab.Screen
+        name="Cart"
+        component={CartScreen}
+        options={{
+          tabBarIcon: ({color, size}) => (
+            <Ionicons name="cart-outline" color={color} size={size} />
+          ),
+        }}
+      />
+      <Tab.Screen
+        name="Settings"
+        component={SettingsScreen}
+        options={{
+          tabBarIcon: ({color, size}) => (
+            <Ionicons name="settings-outline" color={color} size={size} />
+          ),
+        }}
+      />
     </Tab.Navigator>
   );
 };
@@ -39,6 +71,7 @@ const UnAuthScreen = () => {
 
 const Navigate = () => {
   const nameCtx = useContext(NameContext);
+  nameCtx.getStoreData('name');
 
   return (
     <NavigationContainer>
@@ -49,7 +82,7 @@ const Navigate = () => {
 
 export default App = () => {
   return (
-    <SafeAreaView style={{ flex: 1 }}>
+    <SafeAreaView style={{flex: 1}}>
       <NameContextProvider>
         <Navigate />
       </NameContextProvider>
