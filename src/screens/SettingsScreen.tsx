8f804be0ac2a0f82d416d6be38aba110e7e74cb7
Arvind Patel <arvind.patel@thoughtworks.com>
2024-03-28 11:58:08 +0530

    Added Styling to button

--- src/screens/SettingsScreen.tsx
+++ src/screens/SettingsScreen.tsx
@@ -1,17 +1,46 @@
-import {Text, StyleSheet, View, Button, useColorScheme} from 'react-native';
-import {useContext} from 'react';
+import React from 'react';
+import {
+  Text,
+  StyleSheet,
+  View,
+  useColorScheme,
+  Button,
+  TouchableOpacity,
+} from 'react-native';
+import {useContext, useState} from 'react';
 import {useTheme} from '@react-navigation/native';
 import {NameContext} from '../Utils/name-context';
 import MaterialIcons from 'react-native-vector-icons/MaterialIcons';
+import {TextInput} from 'react-native-gesture-handler';
 
 const SettingsScreen = () => {
-  const storeCtx = useContext(NameContext);
   const {colors} = useTheme();
   const scheme = useColorScheme();
+  const {name, getStoreData, setStoreData} = useContext(NameContext);
+  const [isInputDisplay, setInputDisplay] = useState(false);
+  const [newName, setNewName] = useState('');
 
   const themeHandler = () => {
     const setTheme = scheme === 'dark' ? 'light' : 'dark';
-    storeCtx.setStoreData('theme', setTheme);
+    setStoreData('theme', setTheme);
+  };
+
+  const editNameHandler = () => {
+    setInputDisplay(true);
+  };
+
+  const textHandler = (value: string) => {
+    setNewName(value);
+  };
+
+  const saveHandler = () => {
+    setStoreData('name', newName);
+    setInputDisplay(false);
+  };
+
+  const cancelHandler = () => {
+    setInputDisplay(false);
+    setNewName(name.name);
   };
 
   return (
@@ -28,6 +57,38 @@ const SettingsScreen = () => {
       <Text style={[styles.text, {color: colors.text}]}>
         The Text color changes according to the Theme..!
       </Text>
+
+      <View>
+        <Text style={styles.text}> Your Name is : - {name.name}</Text>
+
+        <View style={styles.buttonContainer}>
+          <Button
+            title="edit your name"
+            onPress={editNameHandler}
+            disabled={isInputDisplay}
+          />
+        </View>
+        {isInputDisplay && (
+          <View>
+            <TextInput
+              style={styles.textfiled}
+              placeholder="Enter your name"
+              value={newName}
+              onChangeText={textHandler}
+            />
+
+            <View style={styles.buttonRow}>
+              <TouchableOpacity style={styles.button} onPress={cancelHandler}>
+                <Text style={styles.buttonText}>Cancel</Text>
+              </TouchableOpacity>
+
+              <TouchableOpacity style={styles.button} onPress={saveHandler}>
+                <Text style={styles.buttonText}>Save</Text>
+              </TouchableOpacity>
+            </View>
+          </View>
+        )}
+      </View>
     </>
   );
 };
@@ -44,5 +105,35 @@ const styles = StyleSheet.create({
     fontSize: 20,
     textAlign: 'center',
     marginVertical: 20,
+  },
+
+  buttonContainer: {
+    marginHorizontal: 80,
+    marginBottom: 10,
+  },
+  buttonRow: {
+    flexDirection: 'row',
+    justifyContent: 'center',
+  },
+  button: {
+    borderRadius: 20,
+    backgroundColor: 'blue', // Change to your desired color
+    paddingHorizontal: 20,
+    paddingVertical: 10,
+    marginHorizontal: 5,
+  },
+  buttonText: {
+    color: 'white', // Change to your desired color
+    fontSize: 16,
+    textAlign: 'center',
+  },
+  textfiled: {
+    borderRadius: 8,
+    borderColor: 'gray',
+    borderWidth: 2,
+    marginHorizontal: 80,
+    marginVertical: 20,
+    paddingLeft: 10,
+    height: 45,
   },
 });
